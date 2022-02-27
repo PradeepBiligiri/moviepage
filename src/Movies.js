c0eacea9eed98e4ebfe4d72b1d8fba87b4127e34
PradeepBiligiri <bhadripradeepbiligiri@gmail.com>
2022-02-27 16:58:35 +0530

    Add movie list

--- src/Movies.js
+++ src/Movies.js
@@ -12,6 +12,109 @@ export function Movies({
   // const [enable, setEnable] = useState(false);
   // const toggelVisibility = () => setEnable(!enable);
   const [show, setShow] = useState(true);
+  const [addMovie, setAddMovie] = useState([
+    {
+      name: "Garuda Gamana Vrushabha Vahana",
+      poster:
+        "https://lyricsraag.com/wp-content/uploads/2021/12/Chandrachooda-Garuda-Gamana-Vrishabha-Vahana-lyrics.jpg",
+      rating: 8.6,
+      summary:
+        "Set in the backdrop of the coastal and cultural city of modern-day Mangaluru, Hari and his soulmate Shiva rise together to great heights only to face off as bitter enemies resulting to their own downfall and destruction.",
+    },
+    {
+      name: "Ulidavaru Kandanthe",
+      poster:
+        "https://i.pinimg.com/originals/aa/1f/92/aa1f92bfa7a05deaa3302591fa148216.jpg",
+      rating: 8.4,
+      summary:
+        "A journalist sets out to uncover the truth behind an incident, through the perspectives of different people, unraveling how they and their lives are intertwined with it.",
+    },
+    {
+      name: "Mahaan",
+      poster: "https://pbs.twimg.com/media/E9PolQEVoAEOjzn.jpg",
+      rating: 8.1,
+      summary:
+        "Gandhi Mahaan, a school teacher, is abandoned by his family after he decides to live a life of his own, with personal freedom.",
+    },
+    {
+      name: "Minnal Murali",
+      poster:
+        "https://www.nowrunning.com/content/movie/2018/minna-23659/Stills/minnal_2021320.jpg",
+      rating: 8.1,
+      summary:
+        "An unusual event creates a lightning which in turns gives superhuman abilies to an ambitious tailor ,whose responsibility is now to protect his home village from the evil plans of the antagonist.",
+    },
+    {
+      name: "Spider-Man: No Way Home",
+      poster:
+        "https://mir-s3-cdn-cf.behance.net/project_modules/fs/b8320c110356853.6066eec3b3637.jpg",
+      rating: 8.7,
+      summary:
+        "With Spider-Man's identity now revealed, Peter asks Doctor Strange for help. When a spell goes wrong, dangerous foes from other worlds start to appear, forcing Peter to discover what it truly means to be Spider-Man.",
+    },
+    {
+      name: "RRR",
+      poster:
+        "https://englishtribuneimages.blob.core.windows.net/gallary-content/2021/6/Desk/2021_6$largeimg_977224513.JPG",
+      rating: 8.8,
+      summary:
+        "RRR is an upcoming Indian Telugu-language period action drama film directed by S. S. Rajamouli, and produced by D. V. V. Danayya of DVV Entertainments.",
+    },
+    {
+      name: "Iron man 2",
+      poster:
+        "https://m.media-amazon.com/images/M/MV5BMTM0MDgwNjMyMl5BMl5BanBnXkFtZTcwNTg3NzAzMw@@._V1_FMjpg_UX1000_.jpg",
+      rating: 7,
+      summary:
+        "With the world now aware that he is Iron Man, billionaire inventor Tony Stark (Robert Downey Jr.) faces pressure from all sides to share his technology with the military. He is reluctant to divulge the secrets of his armored suit, fearing the information will fall into the wrong hands. With Pepper Potts (Gwyneth Paltrow) and Rhodes (Don Cheadle) by his side, Tony must forge new alliances and confront a powerful new enemy.",
+    },
+    {
+      name: "No Country for Old Men",
+      poster:
+        "https://upload.wikimedia.org/wikipedia/en/8/8b/No_Country_for_Old_Men_poster.jpg",
+      rating: 8.1,
+      summary:
+        "A hunter's life takes a drastic turn when he discovers two million dollars while strolling through the aftermath of a drug deal. He is then pursued by a psychopathic killer who wants the money.",
+    },
+    {
+      name: "Jai Bhim",
+      poster:
+        "https://m.media-amazon.com/images/M/MV5BY2Y5ZWMwZDgtZDQxYy00Mjk0LThhY2YtMmU1MTRmMjVhMjRiXkEyXkFqcGdeQXVyMTI1NDEyNTM5._V1_FMjpg_UX1000_.jpg",
+      summary:
+        "A tribal woman and a righteous lawyer battle in court to unravel the mystery around the disappearance of her husband, who was picked up the police on a false case",
+      rating: 8.8,
+    },
+    {
+      name: "The Avengers",
+      rating: 8,
+      summary:
+        "Marvel's The Avengers (classified under the name Marvel Avengers\n Assemble in the United Kingdom and Ireland), or simply The Avengers, is\n a 2012 American superhero film based on the Marvel Comics superhero team\n of the same name.",
+      poster:
+        "https://terrigen-cdn-dev.marvel.com/content/prod/1x/avengersendgame_lob_crd_05.jpg",
+    },
+    {
+      name: "Interstellar",
+      poster: "https://m.media-amazon.com/images/I/A1JVqNMI7UL._SL1500_.jpg",
+      rating: 8.6,
+      summary:
+        "When Earth becomes uninhabitable in the future, a farmer and ex-NASA\n pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team\n of researchers, to find a new planet for humans.",
+    },
+    {
+      name: "Baahubali",
+      poster: "https://flxt.tmsimg.com/assets/p11546593_p_v10_af.jpg",
+      rating: 8,
+      summary:
+        "In the kingdom of Mahishmati, Shivudu falls in love with a young warrior woman. While trying to woo her, he learns about the conflict-ridden past of his family and his true legacy.",
+    },
+    {
+      name: "Ratatouille",
+      poster:
+        "https://resizing.flixster.com/gL_JpWcD7sNHNYSwI1ff069Yyug=/ems.ZW1zLXByZC1hc3NldHMvbW92aWVzLzc4ZmJhZjZiLTEzNWMtNDIwOC1hYzU1LTgwZjE3ZjQzNTdiNy5qcGc=",
+      rating: 8,
+      summary:
+        "Remy, a rat, aspires to become a renowned French chef. However, he fails to realise that people despise rodents and will never enjoy a meal cooked by him.",
+    },
+  ]);
   // Conditional styling
   const styles = {
     color: rating > 8 ? "green" : "red",
@@ -29,7 +132,7 @@ export function Movies({
           ⭐ {rating}
         </p>
       </div>
-      <h4>{`The show value is : ${show}`}</h4>
+      {/* <h4>{`The show value is : ${show}`}</h4> */}
       <button
         onClick={() => (setShow(!show) ? "Show summary" : "Hide summary")}
       >
@@ -50,3 +153,4 @@ export function Movies({
     </div>
   );
 }
+function AddMovie() {}
